b66d29e9cd6c981587e68ad1954a054d79597dfd
C856k <Carl856k@stud.kea.dk>
2023-11-09 10:44:43 +0100

    Application is now fully functional, styling is only needed

--- src/main/java/com/example/herbfinder/api/Herbcontroller.java
+++ src/main/java/com/example/herbfinder/api/Herbcontroller.java
@@ -1,21 +1,21 @@
 package com.example.herbfinder.api;
 
 import com.example.herbfinder.dtos.MyResponse;
-import com.example.herbfinder.service.OpenAIService;
+import com.example.herbfinder.service.OpenAiService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/herbs")
 @CrossOrigin(origins = "*")
 public class Herbcontroller {
-    private OpenAIService service;
+    private OpenAiService service;
     final static String SYSTEM_MESSAGE = "You are a high level player with herbalism and alchemy in world of warcraft dragonflight that can provide an answer to where i can find the ingredients, given a user input";
 
-    public Herbcontroller(OpenAIService service) {
+    public Herbcontroller(OpenAiService service) {
         this.service = service;
     }
     @GetMapping
-    public MyResponse getHerb(@RequestParam("about")String about) {
+    public MyResponse getHerb(@RequestParam String about) {
         return service.makeRequest(about,SYSTEM_MESSAGE);
 
     }
--- src/main/resources/static/herbfinder.js
+++ src/main/resources/static/herbfinder.js
@@ -1,30 +1,73 @@
-const SERVER_URL = 'http://localhost:8080';
-document.getElementById("generateButton").addEventListener("click",generateText);
+const SERVER_URL = 'http://localhost:8080/api/';
 
-function generateText() {
-    const userInput = document.getElementById("search-input").value;
+document.getElementById('btn-get-herb').addEventListener('click', getHerb);
 
-    const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
-    const data = {
-        prompt: userInput,
-        max_tokens: 300
-    };
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(result => {
-            const generatedText = result.choices[0].text;
-            document.getElementById("output").textContent = generatedText;
-        })
-        .catch(error => {
-            console.error("API error", error)
+async function getHerb() {
+    const URL = `${SERVER_URL}herbs?about=${document.getElementById('search-input').value}`
+    const result = document.getElementById('result');
+    try {
+        const response = await fetch(URL).then(handleHttpErrors)
+        document.getElementById('result').innerText = response.answer;
+        //document.getElementById('about').value = ''
+    } catch (e) {
+        result.style.color = "red";
+        result.innerText = e.message;
+    }
+}
+
+async function getJokeWithRateLimit() {
+    const URL = `${SERVER_URL}joke2/limited?about= + ${document.getElementById('about2').value}`
+    const result2 = document.getElementById('result2');
+    const spinner2 = document.getElementById('spinner2');
+    result2.style.color = "black";
+    result2.innerText = ""
+    try {
+        spinner2.style.display = "block";
+        const response = await fetch(URL).then(handleHttpErrors)
+        document.getElementById('result2').innerText = response.answer;
+    } catch (e) {
+        result2.style.color = "red";
+        result2.innerText = e.message;
+    } finally {
+        spinner2.style.display = "none";
+    }
+}
+
+async function getInfo() {
+    const URL = `${SERVER_URL}owninfo?question= + ${document.getElementById('the-question').value}`
+    const spinner = document.getElementById('spinner3');
+    const result3 = document.getElementById('result3');
+    result3.innerText = ""
+    result3.style.color = "black";
+    try {
+        spinner.style.display = "block";
+        const reply = await fetch(URL).then(handleHttpErrors)
+        document.getElementById('result3').innerHTML = convertToLink(reply.answer)
+    } catch (e) {
+        result3.style.color = "red";
+        result3.innerText = e.message;
+    } finally {
+        spinner.style.display = "none";
+    }
+
+    function convertToLink(str) {
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        return str.replace(urlRegex, function(match) {
+            if (match.endsWith('.')) {
+                match = match.slice(0, -1); // Remove the trailing dot
+            }
+            return `<a href="${match}" target="_blank">${match}</a>`;
         });
+    }
+}
+
+async function handleHttpErrors(res) {
+    if (!res.ok) {
+        const errorResponse = await res.json();
+        const msg = errorResponse.message ? errorResponse.message : "No error details provided"
+        throw new Error(msg)
+    }
+    return res.json()
 }
 
